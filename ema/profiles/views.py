2f68bf5fa876513361d2d3dfdc95ea64076fee49
Muna <manritter@uos.de>
2016-08-03 17:01:44 +0200

    comments & redirect from index

--- ema/profiles/views.py
+++ ema/profiles/views.py
@@ -1,24 +1,31 @@
 from django import forms
 from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 """
 startpage, link to register and login
+redirect, falls schon eingeloggt zur Matrix
 """
 def index(request):
-    return render(request, 'profiles/index.html')
+    if request.user.is_authenticated():
+        return redirect('/matrix')
+    else: return render(request, 'profiles/index.html')
 
+"""
+wird nach Login angezeigt
+Informationen ueber diesen Benutzer (bis jetzt nur Name) werden angezeigt
+"""
 @login_required(login_url='/account/login')
 def account(request):
     return render(request, 'profiles/account.html')
 
-def loggingin(request):
-    return render(request, 'profiles/login.html')
-
+"""
+Registrierung
+"""
 # http://www.djangobook.com/en/2.0/chapter14.html
 def register(request):
     if request.method == 'POST':
