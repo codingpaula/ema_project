c99020cb73d6247c6b93987a578721bdcef8a6dc
Muna <manritter@uos.de>
2016-08-03 17:04:24 +0200

    new views for editing tested

--- ema/matrix/models.py
+++ ema/matrix/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.db import models
 
@@ -47,3 +48,6 @@ class Task(TimeStampedModel):
     done = models.BooleanField(default=False)
     def __unicode__(self):
         return self.task_name
+
+    # def get_absolute_url(self):
+    #    return reverse('tasks', kwargs={'pk': self.pk})
--- ema/matrix/urls.py
+++ ema/matrix/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.conf.urls import url
 
 from . import views
-from .views import AddTopicView, AddTaskView, TaskUpdate
+from .views import AddTopicView, AddTaskView, TaskUpdate, TaskCreate
 
 urlpatterns = [
     # /matrix/
@@ -17,10 +17,11 @@ urlpatterns = [
     # /matrix/5/tasks
     url(r'^(?P<task_id>[0-9]+)/tasks/$', views.tasks, name='tasks'),
     # /matrix/5/adding
+    # url(r'^(?P<topic_id>[0-9]+)/adding/$', TaskCreate.as_view(), name='adding'),
     url(r'^(?P<topic_id>[0-9]+)/adding/$', AddTaskView.as_view(), name='adding'),
     # /matrix/5/edittopic
     url(r'^(?P<topic_id>[0-9]+)/topicediting/$', views.edittopic,
             name='topicediting'),
     # /matrix/5/tasks/editing
-    url(r'^(?P<task_id>[0-9]+)/taskediting/$', views.editing, name='taskediting')
+    url(r'^(?P<task_id>[0-9]+)/taskediting/$', TaskUpdate.as_view(), name='taskediting')
 ]
