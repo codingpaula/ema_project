ccc698b25e5fc75d4b50f025b34541c5b06db9ca
Manuela Ritter <manuela.ritter@student.uni-luebeck.de>
2017-10-18 21:02:06 +0200

    clean migrations for matrix and orga

--- ema/matrix/migrations/0001_initial.py
+++ ema/matrix/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,20 +16,33 @@ class Migration(migrations.Migration):
             name='Task',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('task_name', models.CharField(max_length=200)),
-                ('task_description', models.TextField()),
-                ('importance', models.CharField(default='1', max_length=1, choices=[('0', 'not important'), ('1', 'less important'), ('2', 'important'), ('3', 'very important')])),
-                ('date_created', models.DateField(auto_now_add=True)),
-                ('date_complete', models.DateField()),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('task_name', models.CharField(max_length=50)),
+                ('task_description', models.TextField(max_length=3000, blank=True)),
+                ('importance', models.IntegerField(default=1, choices=[(0, 'not important'), (1, 'less important'), (2, 'important'), (3, 'very important')])),
+                ('due_date', models.DateTimeField()),
+                ('done', models.BooleanField(default=False)),
+                ('duration', models.FloatField(default=1, blank=True)),
             ],
+            options={
+                'abstract': False,
+            },
         ),
         migrations.CreateModel(
             name='Topic',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
                 ('topic_name', models.CharField(max_length=30)),
-                ('topic_description', models.TextField()),
+                ('topic_description', models.TextField(max_length=2000, blank=True)),
+                ('color', models.CharField(default='black', max_length=15, choices=[('black', 'black'), ('grey', 'grey'), ('rosybrown', 'rosybrown'), ('saddlebrown', 'brown'), ('crimson', 'red'), ('orangered', 'orangered'), ('darkorange', 'orange'), ('gold', 'yellow'), ('yellowgreen', 'lightgreen'), ('olivedrab', 'olive'), ('forestgreen', 'green'), ('darkturquoise', 'turquoise'), ('dodgerblue', 'lightblue'), ('royalblue', 'royalblue'), ('mediumblue', 'blue'), ('hotpink', 'pink'), ('deeppink', 'deeppink'), ('mediumvioletred', 'violet'), ('purple', 'purple'), ('rebeccapurple', 'darkpurple'), ('mediumorchid', 'orchid')])),
+                ('topic_owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+            options={
+                'abstract': False,
+            },
         ),
         migrations.AddField(
             model_name='task',
--- ema/orga/migrations/0001_initial.py
+++ ema/orga/migrations/0001_initial.py
@@ -8,7 +8,6 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('matrix', '0007_auto_20160901_1147'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -19,9 +18,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
-                ('urgent_axis', models.CharField(default=b'1', max_length=1, choices=[(b'0', b'1 month'), (b'1', b'2 months'), (b'2', b'4 months')])),
-                ('tele_username', models.CharField(default=b'', max_length=30, blank=True)),
-                ('default_topic', models.ForeignKey(default=None, to='matrix.Topic', null=True)),
+                ('urgent_axis', models.IntegerField(default=1, choices=[(3, b'1 week'), (4, b'2 weeks'), (0, b'1 month'), (1, b'2 months'), (2, b'4 months')])),
+                ('tele_username', models.TextField(default=None, max_length=30, unique=True, null=True, blank=True)),
                 ('owner', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
